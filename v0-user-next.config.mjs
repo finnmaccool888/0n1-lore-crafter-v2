b99909edba8889007a23212de9d8fd7a5d084e8c
Your Name <your.email@example.com>
2025-04-14 18:33:24 +0000

    Initial commit

---
+++ v0-user-next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['i.imgur.com', 'i.giphy.com', 'media.giphy.com', 'drive.google.com'],
+  },
+}
+
+export default nextConfig
